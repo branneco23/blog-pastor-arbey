bcdf0e23259d927b9809effc09e09cd019e523d9
branneco23 <branmeco22@gmail.com>
2026-04-14 15:27:52 -0500

    Correccion configuracion prisma 3

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-//@ts-expect-error - Prisma CLI types are not available during build
-import { defineConfig } from 'prisma'
-
-export default defineConfig({
-  datasource: {
-    // tu configuración aquí...
-      url: process.env.DATABASE_URL,
-  }
-})
